3af21bdc47425b501eba45ff5dc10426e08d5d31
terman37 <terman37@yahoo.fr>
2020-01-15 19:46:02 +0100

    v0.3

--- AWS-Rekognition/frontend/static/index.js
+++ AWS-Rekognition/frontend/static/index.js
@@ -1,18 +1,26 @@
 function set_webcam(){
-    Webcam.set({
-          width: 320,
-          height: 240,
-          image_format: 'jpeg',
-          jpeg_quality: 90
-     });
+    Webcam.set(
+        {
+            width: 320,
+            height: 240,
+            image_format: 'jpeg',
+            jpeg_quality: 90
+        }
+     );
     Webcam.attach( '#my_camera' );
 }
 
 function take_snapshot() {
     // take snapshot and get image data
-    Webcam.snap( function(data_uri) {
-        // display results in page
-        document.getElementById('results').innerHTML =
-        '<img src="'+data_uri+'"/>';
-    } );
+    Webcam.snap(
+        function(data_uri) {
+            // display results in page
+            document.getElementById('results').innerHTML = '<img src="'+data_uri+'"/>';
+        }
+    );
+}
+
+function recognize() {
+
+
 }
