0a1e5c0cf1bbe689c24c562feb4c1770dd262d0b
terman37 <terman37@yahoo.fr>
2020-01-15 19:08:48 +0100

    v0.2

---
+++ AWS-Rekognition/frontend/static/index.js
@@ -0,0 +1,18 @@
+function set_webcam(){
+    Webcam.set({
+          width: 320,
+          height: 240,
+          image_format: 'jpeg',
+          jpeg_quality: 90
+     });
+    Webcam.attach( '#my_camera' );
+}
+
+function take_snapshot() {
+    // take snapshot and get image data
+    Webcam.snap( function(data_uri) {
+        // display results in page
+        document.getElementById('results').innerHTML =
+        '<img src="'+data_uri+'"/>';
+    } );
+}
