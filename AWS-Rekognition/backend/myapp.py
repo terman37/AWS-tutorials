8fad2e7596940900a11a4350c50e3e0349069933
terman37 <terman37@yahoo.fr>
2020-01-15 20:14:26 +0100

    Update myapp.py

--- AWS-Rekognition/backend/myapp.py
+++ AWS-Rekognition/backend/myapp.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+if __name__ == "__main__":
+# run the app locally on the given port
+    app.run(host='0.0.0.0', port=5000)
