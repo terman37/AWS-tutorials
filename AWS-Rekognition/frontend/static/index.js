595c464de8dbde5023c8cb8c53f9e4c1dd2909a1
terman37 <terman37@yahoo.fr>
2020-01-16 20:57:53 +0100

    Update index.js

--- AWS-Rekognition/frontend/static/index.js
+++ AWS-Rekognition/frontend/static/index.js
@@ -26,7 +26,7 @@ function take_snapshot() {
                 'html'
             );
             function result_f(data_back){
-                $('#results').html(data_back);
+                $('#results').append(data_back);
             }
         }
     );
