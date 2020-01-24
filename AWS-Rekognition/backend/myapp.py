8b9ace24163fa21eca41e6a567ef9c4151793038
terman37 <terman37@yahoo.fr>
2020-01-24 12:56:36 +0100

    Update myapp.py

--- AWS-Rekognition/backend/myapp.py
+++ AWS-Rekognition/backend/myapp.py
@@ -231,8 +231,6 @@ def AWScomparefaces():
 
 def get_features_from_json(myjson):
     # create details table from json response from detect faces
-    print(myjson)
-
     mystr = ""
     facedetails = myjson['FaceDetails']
     nbfaces = len(facedetails)
